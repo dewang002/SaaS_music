cf66404db286079b44fe47bfc9d640f95726b7bc
dewang002 <2002thapa@gmail.com>
2025-03-25 17:13:23 +0530

    done

--- app/creator/[creatorId]/page.tsx
+++ app/creator/[creatorId]/page.tsx
@@ -1,7 +1,9 @@
 "use client"
 import StreamView from "@/app/component/StreamView";
 
-export default function CreatorPage({ params }: { params: { creatorId: string } }) {
-    return <StreamView creatorId={params.creatorId} nowPlaying={false} />;
-  }
-  
+type Params = Promise<{ creatorId: string }>;
+
+export default async function CreatorPage({ params }: { params: Params }) {
+    const { creatorId } = await params;
+    return <StreamView creatorId={creatorId} nowPlaying={false} />;
+}
