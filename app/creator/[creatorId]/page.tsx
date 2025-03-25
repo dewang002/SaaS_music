4c13b940332918bff26d01259e763a9af31e8a61
dewang002 <2002thapa@gmail.com>
2025-03-25 16:27:46 +0530

    creator id

--- app/creator/[creatorId]/page.tsx
+++ app/creator/[creatorId]/page.tsx
@@ -1,14 +1,7 @@
 "use client"
-"use client"
 import StreamView from "@/app/component/StreamView";
 
-interface PageProps {
-  params: {
-    creatorId: string;
-  };
-}
+export default function CreatorPage({params: {creatorId}}:{params: {creatorId: string}}) {
+    return <StreamView creatorId={creatorId} nowPlaying={false} />
 
-export default function CreatorPage({ params }: PageProps) {
-  return <StreamView creatorId={params.creatorId} nowPlaying={false} />;
 }
-
